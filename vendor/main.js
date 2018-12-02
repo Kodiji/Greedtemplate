4d4b329d6fbc2544231e4c50875221b2a11fa5a3
Kodiji Co <Kodiji@users.noreply.github.com>
2018-12-02 18:01:27 +0300

    Update

---
+++ vendor/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function (){
+	
+	
+	$('.js_popup_window').magnificPopup({
+		type: 'inline',
+
+		fixedContentPos: true,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-zoom-in'
+	});
+	
+	$('.js_popup_window_ajax').magnificPopup({
+		type: 'ajax',
+
+		fixedContentPos: true,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-zoom-in'
+	});
+	
+}); 
