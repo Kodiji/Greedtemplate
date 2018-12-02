04c34ae8aa131a6178f01f014e4512e6126a6391
Kodiji Co <Kodiji@users.noreply.github.com>
2018-12-02 20:10:17 +0300

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ $(document).ready(function (){
 		preloader: false,
 
 		midClick: true,
-		removalDelay: 300,
+		removalDelay: 0,
 		mainClass: 'my-mfp-zoom-in'
 	});
 	
@@ -29,7 +29,7 @@ $(document).ready(function (){
 		preloader: false,
 
 		midClick: true,
-		removalDelay: 300,
+		removalDelay: 0,
 		mainClass: 'my-mfp-zoom-in'
 	});
 	
